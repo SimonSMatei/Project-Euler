ee5c4f21cae5c385899d3abf4003b09dd4ba4b5a
Simon Matei <simonsamuelmatei2007@gmail.com>
2026-02-03 22:14:15 -0600

    Refactor prime checking and factorization logic
    Refactor isPrime function to return False for non-prime numbers and simplify factors function to return tuples of factors.

--- Problem-3/problem 3.py
+++ Problem-3/problem 3.py
@@ -2,26 +2,23 @@ from math import sqrt
 
 def isPrime(num):
     factors = 0
+    
     for i in range(1, int(sqrt(num)) + 1):
         if num % i == 0:
             factors += 1
         if factors > 2:
             return False
+    
     if factors == 1:
         return True
+    else:
+        return False
     
 def factors(num):
-    fac = []
-
-    for i in range(1, int(sqrt(num)) + 1):
-        if num % i == 0:
-            fac.append(i)
-            fac.append(num / i)
-    
-    return fac
+    return [(i, num / i) for i in range(1, int(sqrt(num)) + 1) if num % i == 0]
     
 
 if __name__ == "__main__":
     factor = factors(600851475143)
     
-    print(max([i for i in factor if isPrime(i)]))
+    print(max([i for f in factor for i in f if isPrime(i)]))
